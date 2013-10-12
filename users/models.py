d7cbb567c1c857dd3dbb192b5ef8f8110313a78b
jsuper <ling.java@gmail.com>
2013-10-12 17:34:24 +0800

    add user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length=100)
+    genders = models.IntegerField(default=0)
+    birth_date = models.DateField()
+    nick_name = models.CharField(max_length=30)
+    following = models.ManyToManyField('self', symmetrical=False)
