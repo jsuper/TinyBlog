56574bd81bdd63e77f4a0dd2afe46b009034ea90
jsuper <ling.java@gmail.com>
2013-10-14 16:05:47 +0800

    initial new app [blog]

---
+++ blog/models.py
@@ -0,0 +1,19 @@
+import datetime
+
+from django.db import models
+from django import forms
+
+#import user
+from users.models import User
+
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User)
+    content = models.CharField(max_length=140)
+    write_time = models.DateTimeField(default=datetime.datetime.now)
+
+class Comment(models.Model):
+    author = models.ForeignKey(User)
+    content = models.CharField(max_length=140)
+    write_time = models.DateTimeField(default=datetime.datetime.now)
+    post_id = models.ForeignKey(Post)
